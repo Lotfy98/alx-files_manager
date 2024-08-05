5f23f50bf106c02ff9d7f0f3eb94efb8563cedd1
Lotfy98 <mohamed.lotfy78@gmail.com>
2024-08-06 01:06:11 +0300

    0x04. Files manager

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const router = express.Router();
 const AppController = require('../controllers/AppController');
 
-// GET /status
+
 router.get('/status', AppController.getStatus);
 
 // GET /stats
